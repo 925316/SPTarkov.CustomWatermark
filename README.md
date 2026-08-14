2d667e74b97b1e9d6ae7cfbac5af0e4c4485eca1
Bela Proinsias <belaskk@outlook.com>
2026-08-15 01:56:09 +0800

    refactor: rebrand to VersionLabel, migrate to net10 + SPT 4.1.2

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-## Post on [sp-tarkov](https://forge.sp-tarkov.com/mod/2460/belacustomwatermark)
+# VersionLabel
+
+A tiny server-side mod for SPT 4.1.x that replaces the in-game version watermark with the current server version.
+
+## How it works
+
+A Harmony prefix patch (via SPTarkov.Reflection) on `Watermark.GetInGameVersionLabel` returns your server's `CompatibleTarkovVersion` followed by `" Beta version"` instead of the original label.
+
+## Requirements
+
+- [.NET 10 SDK](https://dotnet.microsoft.com/download/dotnet/10.0)
+
+## Build
+
+Double-click `build.bat`, or run:
+
+```powershell
+dotnet build SPTarkov.CustomWatermark.sln -c Release
+```
+
+Output goes to `Build\Release\SPT_Runtime\user\mods\VersionLabel`.
+
+## Install
+
+- Copy the `SPT_Runtime` folder from `Build\Release` into your server root, or
+- Copy only the `VersionLabel` folder into `<server root>\SPT_Runtime\user\mods\`
+
+## License
+
+[AGPL-3.0](LICENSE)
+
+## Post
+
+[sp-mod.com](https://sp-mod.com/,mod/2460/belacustomwatermark)
--- src/main.cs
+++ src/main.cs
@@ -1,3 +1,4 @@
+using SPTarkov.Common.Models.Logging;
 using SPTarkov.DI.Annotations;
 using SPTarkov.Reflection.Patching;
 using SPTarkov.Server.Core.DI;
@@ -8,21 +9,21 @@ using SPTarkov.Server.Core.Servers;
 using SPTarkov.Server.Core.Utils;
 using System.Reflection;
 
-namespace Bela.CustomWatermark;
+namespace VersionLabel;
 
-public record ModMetadata : AbstractModMetadata
+public record ModMetadata : IModMetadata
 {
-    public override string ModGuid { get; init; } = "com.sp-tarkov.Bela.CustomWatermark";
-    public override string Name { get; init; } = "CustomWatermark";
-    public override string Author { get; init; } = "Bela";
-    public override List<string>? Contributors { get; init; }
-    public override SemanticVersioning.Version Version { get; init; } = new("1.0.1");
-    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
-    public override List<string>? Incompatibilities { get; init; }
-    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
-    public override string? Url { get; init; } = "https://github.com/925316/SPTarkov.CustomWatermark";
-    public override bool? IsBundleMod { get; init; }
-    public override string? License { get; init; } = "AGPL-3.0";
+    public string ModGuid { get; init; } = "com.sp.bela.versionlabel";
+    public string Name { get; init; } = "VersionLabel";
+    public string Author { get; init; } = "Bela";
+    public List<string>? Contributors { get; init; }
+    public SemanticVersioning.Version Version { get; init; } = new("1.0.2");
+    public SemanticVersioning.Range SptVersion { get; init; } = new("~4.1.2");
+    public List<string>? Incompatibilities { get; init; }
+    public Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
+    public string? Url { get; init; } = "https://github.com/925316/SPTarkov.CustomWatermark";
+    public string License { get; init; } = "AGPL-3.0";
+    public bool HasPrepatcher { get; init; } = false;
 }
 
 public class ModConfig
@@ -30,22 +31,21 @@ public class ModConfig
     public string? Version { get; set; } = "SPT";
 }
 
-[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
+[Injectable(TypePriority = OnLoadOrder.PostLoad + 1)]
 public class CustomWatermark(
     ISptLogger<CustomWatermark> logger,
-    ConfigServer configServer
+    CoreConfig coreConfig
     )
     : IOnLoad
 {
     private static string? s_version;
 
-    public Task OnLoad()
+    public Task OnLoadAsync(CancellationToken cancellationToken)
     {
-        var coreConfig = configServer.GetConfig<CoreConfig>();
         s_version = coreConfig.CompatibleTarkovVersion;
         s_version += " Beta version";
 
-        logger.Warning($"[Bela.CustomWatermark]: {s_version}");
+        logger.Warning($"[VersionLabel]: {s_version}");
 
         new WatermarkPatch().Enable();
         return Task.CompletedTask;
